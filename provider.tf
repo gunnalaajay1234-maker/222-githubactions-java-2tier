3357257ed3b8ba9b4601837f9b1aac2aab6aec0d
Ajay Goud <gunnalaajay1234@gmail.com>
2026-04-27 00:28:29 +0530

    m1

---
+++ provider.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.5.0"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# This backend block is optional 
+# If you want statefile to store in Storage account use it else remove
+terraform {
+  backend "s3" {
+    bucket         = "saiterrastate"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    use_lockfile = true
+
+  }
+}
